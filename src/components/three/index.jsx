51c9248a13ec9a3eacc7589cea3d6aae6a029a25
unknown <fondolski@gmail.com>
2023-02-21 17:53:00 +0300

    new commit

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -12,7 +12,7 @@ export default function Three() {
         
         <mesh rotation={[-angleToRadians(90), 0 , 0]}>
             <planeGeometry args={[7,7]} />
-            <meshStandardMaterial color='#1ea3d8' transparent opacity={0.3} />
+            <meshStandardMaterial color='#1ea3d8' transparent opacity={0.7} />
          </mesh>
         <ambientLight args={["#ffffff", 1]} />
         </>

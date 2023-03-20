c7c8d5d5c5147c58eb7f10b35e1b49c4b086cfc4
unknown <fondolski@gmail.com>
2023-03-20 17:06:11 +0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, Suspense } from 'react';
 import {Canvas} from '@react-three/fiber';
 import './App.css';
-import Three from './components/three';
+import NewThree from './components/three';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
     <div id='parent-body-container'>
       <Canvas id='three-canvas-container'>
         <Suspense fallback={null}>
-          <Three />
+          <NewThree />
         </Suspense>
       </Canvas>
     </div>
--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,19 +1,38 @@
+import { useState, useEffect } from "react";
 import { angleToRadians } from "../../utils/angle";
-import {PerspectiveCamera} from '@react-three/drei';
+import {PerspectiveCamera, Text, Image, Text3D} from '@react-three/drei';
+import {useLoader} from '@react-three/fiber';
+import * as THREE from 'three';
+import * as Vermin from '../../fonts/Vermin Vibes Mert_Regular.json'
 
-export default function Three() {
+export default function NewThree() {
+
+    useEffect(()=> {
+        console.log(window.innerWidth)
+    },[])
+
     return(
         <>
         <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-        <mesh>
-            <sphereGeometry args={[1, 32, 32]} />
-            <meshStandardMaterial color='#ffffff'/>
-        </mesh>
-        
         <mesh rotation={[-angleToRadians(90), 0 , 0]}>
-            <planeGeometry args={[7,7]} />
-            <meshStandardMaterial color='#1ea3d8' transparent opacity={0.7} />
+            <planeGeometry args={[70, 70]} />
+            <meshStandardMaterial color='#1ea3d8' transparent opacity={0.3} />
          </mesh>
+         <mesh position={[0, 0, 0]}>
+         <Text3D font={Vermin} size={0.3}>
+            react
+            <meshNormalMaterial />
+         </Text3D>
+         </mesh>
+         
+         <Text3D font={Vermin} size={0.3}>
+            <meshNormalMaterial />
+         </Text3D>
+         <mesh>
+            <planeGeometry args={[5, 5]} />
+            <meshStandardMaterial color='#1ea3d8' transparent opacity={0.8}/>
+         </mesh>
+         
         <ambientLight args={["#ffffff", 1]} />
         </>
     )
